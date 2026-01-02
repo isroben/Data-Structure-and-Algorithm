91ba4f53cb93a6eab1a591f06cacd473df25b5f0
Roben <robenthapaaa@gmail.com>
2026-01-02 08:38:36 +0545

    two sum optimized

--- 1_leetcode_twoSum.cpp
+++ 1_leetcode_twoSum.cpp
@@ -23,6 +23,29 @@ vector<int> twoSum(vector<int>& nums, int target) {
 }
 
 // Optimize Approach
+vector<int> twoSum_optim(vector<int>& nums, int target) {
+    int size = nums.size();
+
+    int i = 0, j = size-1;
+    vector<int> ans;
+
+    while(i<j){
+        int pairSum = nums[i] + nums[j];
+        if(pairSum > target){
+            j--;
+        }
+        else if (pairSum > target){
+           i++; 
+        }
+        else{
+            ans.push_back(i);
+            ans.push_back(j);
+            return ans;
+        }
+        
+    }
+    return{};
+}
 
 int main(){
     vector<int> nums = {2, 7, 11, 15};
@@ -31,6 +54,11 @@ int main(){
     vector<int> index = twoSum(nums, target);
     for(int i=0; i<2; i++){
         cout<<index[i]<<"\t"<<endl;;
+    }
+
+    vector<int> ans = twoSum_optim(nums, target);
+    for(int i=0; i<2; i++){
+        cout<<ans[i]<<"\t"<<endl;;
     }
 
     return 0;
