cb803f38aa69e38b3f54283ea4e7b37dc3f577a6
Roben <robenthapaaa@gmail.com>
2026-01-02 08:13:17 +0545

    two sum

---
+++ 1_leetcode_twoSum.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+*/
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    int size = nums.size();
+    for(int st=0; st<size; st++){
+        for(int end=st+1; end<size; end++){
+            if(nums[st]+nums[end]==target){
+                return {st, end};
+            }
+        }
+
+    }
+    return{};
+}
+
+int main(){
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    
+    vector<int> index = twoSum(nums, target);
+    for(int i=0; i<2; i++){
+        cout<<index[i]<<"\t"<<endl;;
+    }
+
+    return 0;
+}
