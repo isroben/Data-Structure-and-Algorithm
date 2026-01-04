d92eb7c032d22232d1cde9606e57b8f6fc3e88b4
Roben <robenthapaaa@gmail.com>
2026-01-04 18:32:24 +0545

    majority elements

--- Majority_elements.cpp
+++ Majority_elements.cpp
@@ -1,15 +1,30 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    int arr[5] = {1, 2, 3, 4, 5};
-    int target = 9;
+/* Majority elements*/
+int majorityElements(int nums[7]){  // Brute force method O(n^2)
 
-    for (int i=0; i<5; i++){
-        for(int j=i+1; j<5; j++){
-            if(arr[i]+arr[j]==target)
-            cout<<i<<j;
+    for(int i=0; i<7; i++){
+        int freq = 0;
+        for(int j=0; j<7; j++){
+            if(nums[i] == nums[j]){
+                freq++;
+            }
+        }
+        cout<<freq<<endl;
+        if(freq>(7/2)){
+            return nums[i];
         }
     }
+}
+
+
+
+int main(){
+    int nums[7] = {2, 2, 1, 1, 1, 2, 2};
+
+    int ans = majorityElements(nums);
+    cout<<ans;
+
     return 0;
 }
