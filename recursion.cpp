ab135b3df137fca253576f03f1f120b2ada2c780
Roben <robenthapaaa@gmail.com>
2026-01-07 07:16:40 +0545

    recursion

---
+++ recursion.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+void printNums(int n){
+    if(n == 1){
+        cout<<n<<"\n";
+        return;
+    }
+    cout<<n<<" ";
+    printNums(n-1);
+}
+
+int factorial(int n){
+    if(n == 0){
+        return 1;
+    }
+    return n * factorial(n-1);
+}
+
+int main(){
+    printNums(5);
+
+    cout<<factorial(5)<<endl;
+
+    return 0;
+}
