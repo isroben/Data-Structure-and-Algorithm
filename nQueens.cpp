6dc3116d7f80472b2f79e4cf464909d753cfb9da
Roben <robenthapaaa@gmail.com>
2026-02-25 16:03:12 +0545

    N Queens Problem

--- nQueens.cpp
+++ nQueens.cpp
@@ -2,6 +2,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+void nQueens(vector<vector<string>> &board, int row, int n, vector<vector<int>> ans){
+    if(row==n){
+        ans.push_back(board);
+        return;
+    }
+
+    for(int j=0; j<n; j++){
+        if(isSafe(board, row, j, n)){
+            board[row][j] = 'Q';
+            nQueens(board, row+1, n, ans);
+            board[row][j] = '.';
+        }
+    }
+}
+
 int main(){
 
 
