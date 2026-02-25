a942da5a5700c4e6d481efaaa0ecb086d0bd58ad
Roben <robenthapaaa@gmail.com>
2026-02-25 16:59:00 +0545

    N Queens Problem

--- nQueens.cpp
+++ nQueens.cpp
@@ -2,6 +2,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+bool isSafe(vector<vector<string>> &board, int row, int col, int j, int n){
+    for(int j=0; j<n; j++){
+        if(board[row][j] == 'Q') return false;
+    }
+
+    for(int i=0; i<n; i++){
+        if(board[i][col] == 'Q') return false;
+    }
+
+    for(int i=row, j=col; i>=0 && j>=0; i--, j--){
+        if(board[i][j] == 'Q') return false;
+    }
+
+    for(int i=row, j=col; i>=0 && j<n; i--, j++){
+        if(board[i][j] == 'Q') return false;
+    }
+    return true;
+}
+
 
 void nQueens(vector<vector<string>> &board, int row, int n, vector<vector<int>> ans){
     if(row==n){
