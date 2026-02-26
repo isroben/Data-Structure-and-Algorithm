cd095047aa8cb0fff17ba27b29d5b7b7cea37d0e
Roben <robenthapaaa@gmail.com>
2026-02-26 14:32:17 +0545

    Rat in Maze problem

--- ratInMaze.cpp
+++ ratInMaze.cpp
@@ -2,22 +2,31 @@
 #include<vector>
 using namespace std;
 
-void helper(vector<vector<int>> &mat, int r, int c, string path, vector<string> &ans){
+void helper(vector<vector<int>> &mat, int r, int c, string path, vector<string> &ans, vector<vector<bool>> &vis){
     int n = mat.size();
-    if(r<0 || c<0 || r >= n || c >= n || mat[r][c] == 0){
+    if(r<0 || c<0 || r >= n || c >= n || mat[r][c] == 0 || vis[r][c] == true){
         return;
     }
+    if(r==n-1 && c == n-1){
+        ans.push_back(path);
+        return;
+    }
+
+    vis[r][c] == true;
     
-    helper(mat, r+1, c, path+"D", ans); // down
-    helper(mat, r-1, c, path+"U", ans); // up
-    helper(mat, r, c+1, path+"R", ans); // right
-    helper(mat, r, c-1, path+"L", ans); // left
+    helper(mat, r+1, c, path+"D", ans, vis); // down
+    helper(mat, r-1, c, path+"U", ans, vis); // up
+    helper(mat, r, c+1, path+"R", ans, vis); // right
+    helper(mat, r, c-1, path+"L", ans, vis); // left
 }
 
 vector<string> findPath(vector<vector<int>> &mat){
+    int n = mat.size();
+
+    vector<vector<bool>> vis(n, vector<bool>(n, false));
     vector<string> ans;
     string path = "";
-    helper(mat, 0, 0, path, ans);
+    helper(mat, 0, 0, path, ans, vis);
 
     return ans;
 }
