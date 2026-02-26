640b8f548fa49f3404103f1acf5dc639e8f8bf8f
Roben <robenthapaaa@gmail.com>
2026-02-26 21:41:42 +0545

    Object oriented programming

---
+++ oops.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Teacher {
+    string name;
+    string dept;
+    string subject;
+    double salary;
+
+    void changeDept(string newDept){
+        dept = newDept;
+    }
+};
+
+class Student {
+    string name;
+    int roll;
+    int age;
+
+};
+
+int main(){
+
+
+
+    return 0;
+}
