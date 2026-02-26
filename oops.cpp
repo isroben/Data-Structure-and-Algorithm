4e79c941afd6575e50e79548139da0308338c7f1
Roben <robenthapaaa@gmail.com>
2026-02-26 21:51:15 +0545

    getter and setter

--- oops.cpp
+++ oops.cpp
@@ -3,13 +3,24 @@
 using namespace std;
 
 class Teacher {
+private:
+    double salary;
+
+public:
     string name;
     string dept;
     string subject;
-    double salary;
 
     void changeDept(string newDept){
         dept = newDept;
+    }
+    // setter --> sets privates values
+    void setSalary(double s){
+        salary = s;
+    }
+    // getter --> gets the values of private values
+    double getSalary(){
+        return salary;
     }
 };
 
@@ -17,12 +28,17 @@ class Student {
     string name;
     int roll;
     int age;
-
 };
 
 int main(){
 
+    Teacher t1;
+    t1.name = "Roben";
+    t1.subject = "Mathematics";
+    t1.dept = "Computer Science";
+    t1.salary = 57000;
 
+    cout << t1.name<<endl;
 
     return 0;
 }
