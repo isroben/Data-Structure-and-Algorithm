2b2cd9a2e66c533fd06ec1fe797558c045f4f1b0
Roben <robenthapaaa@gmail.com>
2026-03-03 14:38:26 +0545

    Constructors and it's types

--- oops.cpp
+++ oops.cpp
@@ -7,8 +7,16 @@ private:
     double salary;
 
 public:
-    Teacher(){  //Constructor
-        cout<<"Hi, I am constructor\n";
+    // Teacher(){  // Non-parameterized or default Constructor;
+    //     cout<<"Hi, I am constructor\n";
+    // }
+
+    Teacher(string n, string d, string s, double sal){ // Parameterized constructor;
+        name = n;
+        dept = d;
+        subject = s;
+        salary = sal;
+
     }
 
     string name;
@@ -46,14 +54,18 @@ public:
 };
 
 int main(){
+    // For Non-parameterized Constructors;
+    // Teacher t1;  //constructor call
+    // t1.name = "Roben";
+    // t1.subject = "Mathematics";
+    // t1.dept = "Computer Science";
+    // t1.setSalary(57000);
 
-    Teacher t1;  //constructor call
-    t1.name = "Roben";
-    t1.subject = "Mathematics";
-    t1.dept = "Computer Science";
-    t1.setSalary(57000);
+    // For Parameterized Constructor;
+    Teacher t1("Shyam", "Mathematics", "Linear Algebra", 57000);
 
     cout << t1.name<<endl;
+    cout << t1.subject<<endl;
     cout << t1.getSalary()<<endl;
 
     return 0;
