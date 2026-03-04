23fc24285b125644af656e01b7e73c5a6659c441
Roben <robenthapaaa@gmail.com>
2026-03-04 15:51:16 +0545

    Levels of inheritance

--- inheritance.cpp
+++ inheritance.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-
+#include<string>
 #include<bits/stdc++.h>
 
 using namespace std;
@@ -7,19 +7,54 @@ using namespace std;
 class Person {
 public:
     string name;
-    int age;
+    string subject;
+};
 
-    Person(string name, int age){
+
+class Teacher : public Person{
+public:
+
+    double salary;
+
+    Teacher(string name, string subject, double salary){
         this->name = name;
-        this->age = age;
+        this->subject = subject;
+        this->salary = salary;
     }
 
+    Teacher(){
+        cout<< "Parent Constructors\n";
+    }
+
+};
+
+
+class Students : public Teacher{
+public:
+    double roll;
+
+    Students(string name, int age, double roll): public Teacher(name, age){
+        this->roll = roll;
+        cout<< "Child Constructors\n";
+    }
+
+    void getInfo(){
+        cout<< "Name of Pet: "<< name<<endl;
+        cout<< "Age of Pet: "<< subject<<endl;
+        cout<< "Category of Pet: "<< roll<<endl;
+    }
+};
+
+
+class TeacherAssistance : public Teacher, public Students {
 
 };
 
 
 int main(){
+    TeacherAssistance p1 ("Musk", 28, "Particle Physics");
+    p1.getInfo();
 
-
+    
     return 0;
 }
