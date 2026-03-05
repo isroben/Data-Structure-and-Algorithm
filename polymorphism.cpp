b2b42d7df603f934a930a9c8c17c98cbddf0a833
Roben <robenthapaaa@gmail.com>
2026-03-05 16:19:14 +0545

    Functions Overriding

--- polymorphism.cpp
+++ polymorphism.cpp
@@ -26,12 +26,30 @@ public:
     }
 };
 
+// Function Overriding
+class Parent {
+public:
+    void getInfo(){
+        cout << "Parent class\n";
+    }
+};
+
+class Child : public Parent{
+public:
+    void getInfo(){
+        cout << "Child class\n";
+    }
+};
+
 int main(){
     Student s1("Tony Stark");
 
     Print p1;
     p1.show(101);
     p1.show('H');
+
+    Child c1;
+    c1.getInfo();
 
     return 0;
 }
