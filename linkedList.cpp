61206bfc49fec45417c6bff76158d964452db8c6
Roben <robenthapaaa@gmail.com>
2026-03-06 16:07:13 +0545

    Front and Back push

--- linkedList.cpp
+++ linkedList.cpp
@@ -29,12 +29,47 @@ public:
 
         if(head == NULL){
             head = tail = newNode;
+            return;
         }
+        else {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+    void push_back(int val){
+        Node* newNode = new Node(val);
+        if(head == NULL){
+            head = tail = newNode;
+            return;
+        }
+        else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void printLL() {
+        Node* temp = head;
+
+        while(temp != NULL){
+            cout<< temp->data << " ";
+            temp = temp->next;
+        }
+        cout<<endl;
     }
 };
 
 int main(){
     List ll;
 
+        ll.push_front(1);
+        ll.push_front(4);
+        ll.push_front(7);
+
+        ll.push_back(0);
+        ll.push_back(9);
+        ll.push_back(8);
+
+        ll.printLL();
     return 0;
 }
