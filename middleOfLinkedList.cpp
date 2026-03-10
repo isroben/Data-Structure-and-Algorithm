1047f3fb3a1134e09c7500babbeb05fe847610a5
isroben <robenthapaaa@gmail.com>
2026-03-10 13:14:18 +0545

    Middle of linked list

--- middleOfLinkedList.cpp
+++ middleOfLinkedList.cpp
@@ -24,8 +24,43 @@ ListNode* middleNode(ListNode *head){
     return slow;
 }
 
+class List {
+    ListNode *head;
+    ListNode *tail;
+    
+public:
+    List(){
+        head = tail = NULL;
+    }
+
+    void pop_front(int val){
+        ListNode* newNode = new ListNode(val);
+
+        if(head == NULL){
+            head = tail = newNode;
+            return;
+        }
+        else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+    ListNode* gethead(){
+        return head;
+    }
+};
+
 
 int main(){
+    List ll;
+    
+    ll.pop_front(1);
+    ll.pop_front(2);
+    ll.pop_front(3);
+    ll.pop_front(4);
+    ll.pop_front(5);
 
+    cout<< middleNode(ll.gethead())->data<<endl;
+    
     return 0;
 }
