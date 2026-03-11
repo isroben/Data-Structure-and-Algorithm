b6327df8526c38ec1b227b576aeb8d203c1d3d0f
isroben <robenthapaaa@gmail.com>
2026-03-11 15:58:14 +0545

    bug fix

--- middleOfLinkedList.cpp
+++ middleOfLinkedList.cpp
@@ -33,7 +33,7 @@ public:
         head = tail = NULL;
     }
 
-    void push_front(int val){
+    void push_back(int val){
         ListNode* newNode = new ListNode(val);
 
         if(head == NULL){
@@ -54,11 +54,11 @@ public:
 int main(){
     List ll;
     
-    ll.push_front(1);
-    ll.push_front(2);
-    ll.push_front(3);
-    ll.push_front(4);
-    ll.push_front(5);
+    ll.push_back(1);
+    ll.push_back(2);
+    ll.push_back(3);
+    ll.push_back(4);
+    ll.push_back(5);
 
     cout<< middleNode(ll.gethead())->data<<endl;
     
