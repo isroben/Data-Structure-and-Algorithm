b163774eb772843095adfda5ae354234e7a67bf5
isroben <robenthapaaa@gmail.com>
2026-03-11 16:12:12 +0545

    Detect Cycle in Linked List

--- cycleInLL.cpp
+++ cycleInLL.cpp
@@ -22,12 +22,47 @@ public:
     List(){
         head = tail = NULL;
     }
-    void push_front(int val){
+    void push_back(int val){
         ListNode* newNode = new ListNode(val);
 
         if(head == NULL){
             head = tail = newNode;
             return;
         }
+        else{
+            tail->next = newNode;
+            tail = newNode;
+        }
     }
+    ListNode* getHead(){
+        return head;
+    }
+};
+
+bool hasCycle(ListNode* head){
+    ListNode* slow;
+    ListNode* fast;
+
+    slow = fast = head;
+    while(fast != NULL && fast->next != NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+        
+        if(slow == fast) return true;
+    }
+    return false;
+}
+
+
+int main(){
+    List ll;
+
+    ll.push_back(3);
+    ll.push_back(2);
+    ll.push_back(0);
+    ll.push_back(-4);
+
+    cout<< hasCycle(ll.getHead()->next) <<endl;
+
+    return 0;
 }
