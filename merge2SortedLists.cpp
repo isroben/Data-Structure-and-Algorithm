d1ff9465e7dd49b4953c32267f0932f4ad9a9907
isroben <robenthapaaa@gmail.com>
2026-03-13 18:56:22 +0545

    Merge two sorted list

---
+++ merge2SortedLists.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+ListNode* mergeTwoLists(ListNode* h1, ListNode* h2) {
+    if(h1 == NULL || h2 == NULL){
+        return head1 == NULL ? head2 : head1;
+    }
+
+    if(h1->val <= h2->val){
+        h1->next = mergeTwoLists(h1->next, h2);
+        return h1;
+    }
+    else{
+        h2->next = mergeTwoLists(h1, h2->next);
+        return h2;
+    }
+}
