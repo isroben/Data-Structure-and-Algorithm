87fcf0ed589dadf84d6b7b7ee6619a15e1ee7212
isroben <robenthapaaa@gmail.com>
2026-03-14 20:51:08 +0545

    Doubly linked list, push front and push pack

--- doublyLinkedList.cpp
+++ doublyLinkedList.cpp
@@ -4,27 +4,74 @@
 using namespace std;
 
 class Node{
+public:
     int data;
     Node* next;
     Node* prev;
 
     Node(int val){
         data = val;
-        Node* next = NULL;
-        Node* prev = NULL;
+        next = prev =  NULL;
     }
 };
 
 class doublyList{
     Node* head;
     Node* tail;
-
+public:
     doublyList(){
         head = tail = NULL;
     }
     void push_front(int val){
+        Node* newNode = new Node(val);
+
         if(head == NULL){
-            
+            head = tail = newNode;
+        }
+        else{
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
+    }
+    void push_back(int val){
+        Node* newNode = new Node(val);
+
+        if(head == NULL){
+            head = tail = newNode;
+        }
+        else{
+            newNode->prev = head;
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void print(){
+        Node* temp = head;
+
+        while(temp != NULL){
+            cout<< temp->data <<endl;
+            temp = temp->next;
         }
     }
 };
+
+int main(){
+    doublyList ll;
+
+    ll.push_front(2);
+    ll.push_front(5);
+    ll.push_front(8);
+
+    ll.print();
+
+    ll.push_back(9);
+    ll.push_back(3);
+    ll.push_back(1);
+
+
+    ll.print();
+
+    return 0;
+}
