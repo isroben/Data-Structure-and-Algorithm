e16942cfe490358481cf60abca4b1d600126ced6
isroben <robenthapaaa@gmail.com>
2026-03-19 12:32:19 +0545

    Swaping Nodes in Paris, Leetcode problem

--- swapNodesInPairs.cpp
+++ swapNodesInPairs.cpp
@@ -9,7 +9,26 @@ struct ListNode{
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-ListNode* swapPairs(ListNode* head) {}
+ListNode* swapPairs(ListNode* head) {
+    if(head == NULL || head->next == NULL){
+        return head;
+    }
+    ListNode* first = head; ListNode* sec = head->next;
+    ListNode* prev = NULL;
+
+    while(first != NULL && sec != NULL){
+        ListNode* third = sec->next;
+        sec->next = first;
+        first->next = third;
+
+        prev != NULL ? prev->next = sec : head = sec;
+
+        prev = first;
+        first = third;
+        third != NULL ? sec = third->next : sec = NULL;
+    }
+    return head;
+}
 
 int main(){
 
