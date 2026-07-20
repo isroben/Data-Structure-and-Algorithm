f89bb26eb8c543550a26de1062b7af8792e41f26
isroben <robenthapaaa@gmail.com>
2026-07-20 22:46:07 +0545

    Pre-order and In-order traversal techniques

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -27,12 +27,35 @@ Node* buildTree(vector<int> preOrder){
     return root;
 }
 
+// Tree Traversal -> preorder
+void traversalTree(Node* root){
+    if(root == NULL){
+        cout<< -1 <<" ";
+        return;
+    }
+
+    cout<< root->data <<" ";
+    traversalTree(root->left);
+    traversalTree(root->right);
+}
+
+// InOrder Traversal
+void inOrder(Node* root){
+    if(root == NULL) return;
+
+    inOrder(root->left);
+    cout<< root->data <<" ";
+    inOrder(root->right);
+}
+
 int main(){
     vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
 
     Node* root = buildTree(preorder);
 
-    cout<< root->left->data <<endl;
+    cout<<"Pre-Order: ",traversalTree(root);
+    cout<<endl;
+    cout<<"In-Order: ",inOrder(root);
 
     return 0;
 }
