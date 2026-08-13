daea4822e9f20e3db6bc1ab1281ec47c0ed66b5b
isroben <robenthapaaa@gmail.com>
2026-08-13 21:01:12 +0545

    Smart pointers in C++

---
+++ smartPointers.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <memory>
+using namespace std;
+
+void function(){
+    unique_ptr<int> p1 (new int(33)); // creating unique pointer
+
+    p1.reset(new int(10)); // to reset the value stored
+
+    unique_ptr<int> p2;
+
+    p2 = std::move(p1); // moving ownerships
+
+    // cout<< *p1 <<endl; // gives segmt fault
+    cout<< *p2 <<endl;
+}
+
+
+int main(){
+
+    function();
+
+    return 0;
+}
